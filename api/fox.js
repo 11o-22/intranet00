af6faf4f32e4e66e2b584a29057d9142dce6fd62
11o-22 <y3875038@gmail.com>
2026-09-17 17:16:18 +0900

    zz

--- api/fox.js
+++ api/fox.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
                 'X-Title': 'Qtrew Intranet'
             },
             body: JSON.stringify({
-         model : 'sao10k/l3-lunaris-8b',
+         model : 'cognitivecomputations/dolphin-llama-3-8b',
                 max_tokens: 600,
                 temperature: 0.9,
                 messages: fullMessages
