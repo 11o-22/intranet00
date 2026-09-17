54efe5548c6b10cd6884fe5dee392bc8ddc76cd6
11o-22 <y3875038@gmail.com>
2026-09-17 16:50:56 +0900

    z

--- api/fox.js
+++ api/fox.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
                 'X-Title': 'Qtrew Intranet'
             },
             body: JSON.stringify({
-         model : 'sao10k/fimbulvetr-11b-v2',
+         model : 'undi95/toppy-m-7b',
                 max_tokens: 600,
                 temperature: 0.9,
                 messages: fullMessages
