954e2881b78f1203a045deccec4ce8742eeebf05
11o-22 <y3875038@gmail.com>
2026-09-24 21:50:26 +0900

    ㅋㅋ

--- dark.js
+++ dark.js
@@ -2890,7 +2890,6 @@ function input119D(n) {
                 darkRun._advTo = null;        
                 detachVoteListener();
                 renderDarkStep();
-                if (darkRun.step !== cur) { darkRun.step = cur; renderDarkStep(); }
             }
         });
 
